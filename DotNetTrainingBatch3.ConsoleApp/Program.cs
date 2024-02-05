b5f08fe9c10d61b36eeb6104a81103a310411de5
nyi23976 <nyi23976@gmail.com>
2024-02-05 16:50:13 +0630

    Add project files.

---
+++ DotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http.Headers;
+
+#region for read
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-JRQ534H\\MSSQLSERVER2019";
+stringBuilder.InitialCatalog = "TestDb";
+stringBuilder.UserID = "sa";
+stringBuilder.Password="sasa"; 
+
+SqlConnection sqlConnection = new SqlConnection(stringBuilder.ConnectionString);
+
+sqlConnection.Open();
+string Queryable = "select * from tbl_Blog";
+SqlCommand sqlCommand = new SqlCommand(Queryable, sqlConnection);
+SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+sqlConnection.Close();
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["blogid"]);
+    Console.WriteLine(dr[1]);
+    Console.WriteLine(dr[2]);
+    Console.WriteLine(dr["blogcontent"]);
+    Console.WriteLine();
+}
+#endregion
